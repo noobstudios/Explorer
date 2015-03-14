d9dd1698c1984d55c49d63f7ba407392445035df
Nazar <bazarsen@MBP-Nazar.untc.net.ua>
2015-03-14 22:24:27 +0200

    5th line

--- Explorer/AppDelegate.swift
+++ Explorer/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  Explorer
 //
 //  Created by Ian K. on 3/14/15.
-//  Copyright (c) 2015 Noob Studios. All rights reserved.
+//  Copyright (c) 2015 Noob Studios. All rights reserved. Don't even try to copy this!'
 //
 
 import UIKit
