ce8b409645b18456f73c5270c2999d8728d282e0
noobstudios <noobstudiosua@gmail.com>
2015-03-15 20:57:39 +0200

    Background update

--- Explorer/GameScene.swift
+++ Explorer/GameScene.swift
@@ -8,3 +8,16 @@
 
 import SpriteKit
 
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        var bgImage = SKSpriteNode(imageNamed: "background.jpg")
+        bgImage.position = CGPointMake(self.size.width/2, self.size.height/2)
+        self.addChild(bgImage)
+    }
+    
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
